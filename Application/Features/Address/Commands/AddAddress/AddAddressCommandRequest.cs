289a74b8ba560ee58ec44413d852606291223fc9
Mahdi <Mahdii>
2023-12-27 12:25:32 +0330

    Split Add Address Requsest

--- Application/Features/Address/Commands/AddAddress/AddAddressCommandRequest.cs
+++ Application/Features/Address/Commands/AddAddress/AddAddressCommandRequest.cs
@@ -1,6 +1,4 @@
-﻿using Application.IRepositories;
-using AutoMapper;
-using MediatR;
+﻿using MediatR;
 
 namespace Application.Features.Address.Commands.AddAddress;
 
@@ -12,33 +10,4 @@ public class AddAddressCommandRequest : IRequest<bool>
     public string ZipCode { get; set; }
 
     public int CustomerId { get; set; }
-}
-
-
-public class AddAddressCommandRequestHandler : IRequestHandler<AddAddressCommandRequest, bool>
-{
-    private readonly IMapper _mapper;
-    private readonly IAddressRepository _addressRepository;
-    private readonly ICustomerRepository _customerRepository;
-
-    public AddAddressCommandRequestHandler(IMapper mapper, IAddressRepository addressRepository, ICustomerRepository customerRepository)
-    {
-        _mapper = mapper;
-        _addressRepository = addressRepository;
-        _customerRepository = customerRepository;
-    }
-    public async Task<bool> Handle(AddAddressCommandRequest request, CancellationToken cancellationToken)
-    {
-        //var customer = _customerRepository.Get(request.CustomerId, cancellationToken);
-        var isCustomerExist = _customerRepository.IsCustomerExist(request.CustomerId, cancellationToken);
-        if (isCustomerExist.Result)
-        {
-            var address = _mapper.Map<Domain.Models.Address>(request);
-            await _addressRepository.Add(address, cancellationToken);
-            await _addressRepository.Save();
-            return true;
-        }
-        return false;
-
-    }
 }
---
+++ Application/Features/Address/Commands/AddAddress/AddAddressCommandRequestHandler.cs
@@ -0,0 +1,32 @@
+﻿using Application.IRepositories;
+using AutoMapper;
+using MediatR;
+
+namespace Application.Features.Address.Commands.AddAddress;
+
+public class AddAddressCommandRequestHandler : IRequestHandler<AddAddressCommandRequest, bool>
+{
+    private readonly IMapper _mapper;
+    private readonly IAddressRepository _addressRepository;
+    private readonly ICustomerRepository _customerRepository;
+
+    public AddAddressCommandRequestHandler(IMapper mapper, IAddressRepository addressRepository, ICustomerRepository customerRepository)
+    {
+        _mapper = mapper;
+        _addressRepository = addressRepository;
+        _customerRepository = customerRepository;
+    }
+    public async Task<bool> Handle(AddAddressCommandRequest request, CancellationToken cancellationToken)
+    {
+        var isCustomerExist = _customerRepository.IsCustomerExist(request.CustomerId, cancellationToken);
+        if (isCustomerExist.Result)
+        {
+            var address = _mapper.Map<Domain.Models.Address>(request);
+            await _addressRepository.Add(address, cancellationToken);
+            await _addressRepository.Save();
+            return true;
+        }
+        return false;
+
+    }
+}
